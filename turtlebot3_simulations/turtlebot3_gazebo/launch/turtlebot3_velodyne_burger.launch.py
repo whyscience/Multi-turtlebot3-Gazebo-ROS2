94ee967ddd9e8353f82215bd80a1b046288c020c
eric <haiyang.wang@clobotics.com>
2024-02-04 23:27:06 +0800

    format code

--- turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_velodyne_burger.launch.py
+++ turtlebot3_simulations/turtlebot3_gazebo/launch/turtlebot3_velodyne_burger.launch.py
@@ -29,28 +29,30 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch.substitutions import Command
 
-
 TURTLEBOT3_MODEL = "burger"
+
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     world_file_name = 'turtlebot3_velodyne_robots.world'
     world = os.path.join(get_package_share_directory('turtlebot3_gazebo'), 'worlds', world_file_name)
-    robot_desc_path = os.path.join(get_package_share_directory("turtlebot3_description"), "urdf", "turtlebot3_velodyne_burger.urdf")
+    robot_desc_path = os.path.join(get_package_share_directory("turtlebot3_description"), "urdf",
+                                   "turtlebot3_velodyne_burger.urdf")
 
-    entity_name_0=""
+    entity_name_0 = ""
 
     return LaunchDescription([
         ExecuteProcess(
             cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so'],
             output='screen'),
-        
+
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
             namespace=entity_name_0,
-            parameters=[{'frame_prefix': entity_name_0+'/', 'use_sim_time': use_sim_time, 'robot_description': Command(['xacro ', robot_desc_path, ' robot_name:=', entity_name_0])}],
+            parameters=[{'frame_prefix': entity_name_0 + '/', 'use_sim_time': use_sim_time,
+                         'robot_description': Command(['xacro ', robot_desc_path, ' robot_name:=', entity_name_0])}],
             output="screen"
         ),
 
